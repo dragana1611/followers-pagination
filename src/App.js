49faec9bbe72bf8e5904391beeeadb50dcfca66a
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-05-11 01:44:25 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import { useFetch } from "./useFetch";
+import Follower from "./Follower";
+function App() {
+  const { loading, data } = useFetch();
+  const [page, setPage] = useState(0);
+  const [followers, setFollowers] = useState([]);
+
+  useEffect(() => {
+    if (loading) return;
+    setFollowers(data[page]);
+  }, [loading, page]);
+
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if (nextPage > data.length - 1) {
+        nextPage = 0;
+      }
+      return nextPage;
+    });
+  };
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1;
+      if (prevPage < 0) {
+        prevPage = data.length - 1;
+      }
+      return prevPage;
+    });
+  };
+
+  const handlePage = (index) => {
+    setPage(index);
+  };
+
+  return (
+    <>
+      <header>
+        <h1>Followers Pagination</h1>
+      </header>
+      <main>
+        <div className="section-title">
+          <h2>{loading ? "loading..." : "pagination"}</h2>
+          <div className="underline"></div>
+        </div>
+        <section className="followers">
+          <div className="container">
+            {followers.map((follower) => {
+              return <Follower key={follower.id} {...follower} />;
+            })}
+          </div>
+          {!loading && <div className="btn-container">hello</div>}
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
